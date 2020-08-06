6628339cfb87f1c0b7860737c01ec6f80d71c5f2
SanyaKasaudhan <kasaudhansanya@gmail.com>
2020-08-06 13:53:09 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React ,{useEffect, useState} from 'react'; 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () =>{
 
+  const APP_ID ="fa6fdf86";
+  const APP_KEY= "83c74dd3d52d4f1f5af6dbc83bbcbb30";
+  const exampleReq= "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}";
+  const [counter,setCounter] =useState(0);
+  useEffect(()=>{
+    console.log('Effect has been run');
+  })
+  return(
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button onClick={()=> setCounter(counter+1)}
+        className="search-button" type="submit">
+          {counter}
+        </button>
+      </form>
+    </div>
+  )
+}
 export default App;
